d4494d170e7bf73356875ef9668c4893d21bd35e
mellow874 <felicitymncube49@gmail.com>
2025-12-01 17:53:31 +0200

    imported pages, and added routing to files

--- billyg-frontend/src/App.jsx
+++ billyg-frontend/src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Reservation from './components/Reservationdate';
+import Home from './pages/Home'
+import FormDetail from './components/FormDetail';
+import Confirmation from './components/Confirmation';
+import { BookingProvider } from "./components/Booking";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    //Booking Provider for global state
+    <BookingProvider>
+      {/* Router to move between pages */}
+    <Router>
+       <div className='min-h-screen bg-gray-100 flex items-center justify-center p-4'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/reservation" element={<Reservationdate />} />
+        <Route path="/details" element={<FormDetail />} />
+        <Route path="/confirmation" element={<Confirmation />} />
+      </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </Router>
+    </BookingProvider>
   )
 }
 
